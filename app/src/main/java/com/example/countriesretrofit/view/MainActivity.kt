85c19eab3490ebae8091d86594b6bfbaf89442a3
csmith495 <tekF@ce1994>
2022-02-10 16:55:09 -0600

    Retrofit Country Android App

--- app/src/main/java/com/example/countriesretrofit/view/MainActivity.kt
+++ app/src/main/java/com/example/countriesretrofit/view/MainActivity.kt
@@ -14,6 +14,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    // GitHub Test
     lateinit var viewModel: ListViewModel
     private val countriesAdapter = CountryListAdapter(arrayListOf())
 
